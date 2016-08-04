b56219ef90058c750ff17634ed27afda3d3c1890
Tushar <tushar.kadu@gslab.com>
2016-08-04 17:33:53 +0530

    TEst commit

---
+++ TestProject/src/main/java/TestClass.java
@@ -0,0 +1,15 @@
+// Copyright 2010-2014 Oleg Danilov (a.k.a ODA, Novos40) under the terms of 
+// the MIT license found at http://www.opensource.org/licenses/mit-license.html
+
+public class TestClass
+{
+
+public static void
+main(String[] args)
+{
+    // TODO Auto-generated method stub
+    System.out.println("Hello Drone..................");
+
+}
+
+}
